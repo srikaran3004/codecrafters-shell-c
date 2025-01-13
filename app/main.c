86023afddce9916664fdbdca6f21812eae3a634b
Srikaran Devarakonda <srikaran2230@gmail.com>
2025-01-13 10:54:33 +0530

    [REPL]

--- app/main.c
+++ app/main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+    setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
+    char input[100];
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
-  return 0;
+    while (1) {
+        printf("$ ");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strlen(input) == 0) {
+            continue;
+        }
+
+        printf("%s: command not found\n", input);
+    }
+
+    return 0;
 }
