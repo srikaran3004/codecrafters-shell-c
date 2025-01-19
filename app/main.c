24eacd9272a91efa623d003bd7ecb5aa23e419cf
Srikaran Devarakonda <srikaran2230@gmail.com>
2025-01-19 12:53:41 +0530

    [The exit builtin]

--- app/main.c
+++ app/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
     setbuf(stdout, NULL);
@@ -7,15 +8,14 @@ int main() {
     char input[100];
 
     while (1) {
-        printf("$ ");
         if (fgets(input, sizeof(input), stdin) == NULL) {
             break;
         }
 
         input[strcspn(input, "\n")] = '\0';
 
-        if (strlen(input) == 0) {
-            continue;
+        if (strcmp(input, "exit 0") == 0) {
+            exit(0);
         }
 
         printf("%s: command not found\n", input);
